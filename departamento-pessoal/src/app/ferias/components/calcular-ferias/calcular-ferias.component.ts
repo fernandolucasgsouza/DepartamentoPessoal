6b623da649c6ecc8f361ad6779bd35f2699abee9
Fernando Souza <fernandolucass.2012@gmail.com>
2017-12-12 13:37:00 -0200

    calculos

--- departamento-pessoal/src/app/ferias/components/calcular-ferias/calcular-ferias.component.ts
+++ departamento-pessoal/src/app/ferias/components/calcular-ferias/calcular-ferias.component.ts
@@ -11,25 +11,23 @@ import * as s from '../../services';
 export class CalcularFeriasComponent implements OnInit {
 
   public itemFerias: object;
+  public itemFerias1_3: object;
   public formCalculaFerias: FormGroup;
 
   public fbGroup = {
-    salario : new FormControl('', Validators.required),
-    horasExtras : new FormControl('', Validators.required),
-    dias : new FormControl('', Validators.required),
-    dependentes : new FormControl('', Validators.required),
+    salario: new FormControl('', Validators.required),
+    horasExtras: new FormControl('', Validators.required),
+    dias: new FormControl('', Validators.required),
+    dependentes: new FormControl('', Validators.required),
   };
 
   constructor(
     private _fb: FormBuilder,
-    private _service : s.FeriasService
+    private _service: s.FeriasService
   ) {
     this.formCalculaFerias = this._fb.group(this.fbGroup);
-    this.itemFerias = {
-      ref: '-',
-      proventos : '-',
-      descontos : '-'
-    };
+    this.itemFerias = { ref: '-', proventos: '-', descontos: '-' };
+    this.itemFerias1_3 = { ref: '-', proventos: '-', descontos: '-' };
   }
 
   ngOnInit() {
@@ -41,20 +39,26 @@ export class CalcularFeriasComponent implements OnInit {
 
     let salario = parseFloat(this.formCalculaFerias.get('salario').value);
     let horasExtras = parseFloat(this.formCalculaFerias.get('horasExtras').value);
+    let diasFerias = parseInt(this.formCalculaFerias.get('dias').value);
 
-    console.log(salario);
-    console.log(horasExtras);
-    console.log(this._service.calculaSalarioBruto(salario, horasExtras));
+    let valorFeriasBruto = this._service.calculaFerias(salario, horasExtras, diasFerias)
+    let valor1_3 = this._service.calcularFerias1_3(valorFeriasBruto);
 
     this.itemFerias = {
-      ref: '-',
-      proventos : this._service.calculaSalarioBruto(salario, horasExtras),
-      descontos : '-'
+      ref: diasFerias + 'd',
+      proventos: 'R$' + valorFeriasBruto,
+      descontos: '-'
     };
 
+    this.itemFerias1_3 = {
+      ref: '-',
+      proventos: 'R$' + valor1_3,
+      descontos: '-'
+    }
   }
 
-  public clean(){
-    this.itemFerias = { ref: '-', proventos : '-', descontos : '-'};
+  public clean() {
+    this.itemFerias = { ref: '-', proventos: '-', descontos: '-' };
+    this.itemFerias1_3 = { ref: '-', proventos: '-', descontos: '-' };
   }
 }
--- departamento-pessoal/src/app/ferias/services/ferias.service.ts
+++ departamento-pessoal/src/app/ferias/services/ferias.service.ts
@@ -3,11 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FeriasService {
 
+  public result: number;
+
   constructor() { }
 
-  public calculaSalarioBruto(salario: number, horasExtras: number): number {
-    let result = salario + horasExtras;
-    return result;
+  public calculaFerias(salario: number, horasExtras: number, dias:number): number {
+    let valorBruto = salario + horasExtras;
+    this.result = (valorBruto / 30) * dias;
+    return this.result;
+  }
+
+  public calcularFerias1_3(valorBruto:number):number{
+    return valorBruto / 3;
   }
 
 }
