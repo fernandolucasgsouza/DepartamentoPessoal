d92084de7e458e769d06f0091df76a6fe97199ee
Fernando Souza <fernandolucass.2012@gmail.com>
2017-12-28 12:56:41 -0200

    component modal v4

--- departamento-pessoal/src/app/core/components/tabela-inss/tabela-inss.component.ts
+++ departamento-pessoal/src/app/core/components/tabela-inss/tabela-inss.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as ctc from '../../constants/inss'
+
 @Component({
   selector: 'app-tabela-inss',
   templateUrl: './tabela-inss.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabelaInssComponent implements OnInit {
 
-  constructor() { }
+  public listHead:any = []
+  public listBody:any = []
+  private inss: ctc.Inss
+  constructor(
+  ) { }
 
   ngOnInit() {
+    this.createTable();
+  }
+
+  public createTable(){
+    this.listHead = [
+      {name:'Descrição' },
+      {name:'Percentual'}
+    ]
+
+    this.listBody = [
+      {description:`Salário até R$${ctc.Inss.FAIXA_1.MAX}`, percent:`${ctc.Inss.FAIXA_1.PERCENT}%`},
+      {description:`Salário de R$${ctc.Inss.FAIXA_2.MIN} até R$${ctc.Inss.FAIXA_2.MAX}`, percent:`${ctc.Inss.FAIXA_2.PERCENT}%`},
+      {description:`Salário de R$${ctc.Inss.FAIXA_3.MIN} até R$${ctc.Inss.FAIXA_3.MAX}`, percent:`${ctc.Inss.FAIXA_3.PERCENT}%`},
+    ]
   }
 
 }
--- departamento-pessoal/src/app/core/constants/inss.ts
+++ departamento-pessoal/src/app/core/constants/inss.ts
@@ -6,11 +6,13 @@ export class Inss{
   }
 
   static readonly FAIXA_2 = {
+    MIN: "1.659,39",
     MAX: "2.765,66",
     PERCENT: 9
   }
 
   static readonly FAIXA_3 = {
+    MIN: "2.765,67",
     MAX: "5.531,31",
     PERCENT: 11
   }
