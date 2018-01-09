fa74fe80148a25ae5596070d8c20f723d8ce628a
Fernando Souza <fernandolucass.2012@gmail.com>
2018-01-09 13:31:01 -0200

    teste input

--- departamento-pessoal/src/app/core/components/input/input.component.ts
+++ departamento-pessoal/src/app/core/components/input/input.component.ts
@@ -1,21 +1,40 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormControlName } from '@angular/forms';
+import { ControlValueAccessor } from '@angular/forms';
+import { ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'fs-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+    selector: 'fs-input',
+    templateUrl: './input.component.html',
+    styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
-    @Input() label :string;
-    @Input() id :string;
-    @Input() type :string;
-    @Input() placeholder :string;
+export class InputComponent implements OnInit, ControlValueAccessor {
+    @Input() label: string;
+    @Input() id: string;
+    @Input() type: string;
+    @Input() placeholder: string;
     @Input() control: FormControl;
 
-  constructor() { }
+    constructor(private _elRef: ElementRef) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    writeValue(obj: any): void {
+        debugger
+        console.log(obj)
+    }
+    registerOnChange(fn: any): void {
+        debugger
+        console.log(fn)
+    }
+    registerOnTouched(fn: any): void {
+        debugger
+        console.log(fn)
+    }
+    setDisabledState?(isDisabled: boolean): void {
+        throw new Error("Method not implemented.");
+    }
 
 }
