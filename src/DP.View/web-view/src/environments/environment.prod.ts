578ef593f024bdf269f27c3fb699f32c5008495c
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2020-06-13 11:45:34 -0300

    implementation firebase, history inss and irrf, more refactors

--- src/DP.View/web-view/src/environments/environment.prod.ts
+++ src/DP.View/web-view/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCyn3dzYWNFvwqAmof17DOG6hLziHSHww8',
+    authDomain: 'departamentopessoal-65f62.firebaseapp.com',
+    databaseURL: 'https://departamentopessoal-65f62.firebaseio.com',
+    projectId: 'departamentopessoal-65f62',
+    storageBucket: 'departamentopessoal-65f62.appspot.com',
+    messagingSenderId: '8281083134',
+    appId: '1:8281083134:web:ad5fed4d1c275e749a29af',
+    measurementId: 'G-D769ZM65Q3',
+  }
 };
